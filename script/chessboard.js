034a0feac25ce5241f1bf8539e47af37f81f8ff7
Preston Chapman <pchapman82070@uat.edu>
2024-07-02 11:20:03 -0700

    Add Color pickers

--- script/chessboard.js
+++ script/chessboard.js
@@ -1,4 +1,10 @@
-document.addEventListener("DOMContentLoaded", () => createBoard());
+document.addEventListener("DOMContentLoaded", () => main());
+
+function main(){
+    document.getElementById("chessOddColor").addEventListener("change", () => updateColors())
+    document.getElementById("chessEvenColor").addEventListener("change", () => updateColors())
+    createBoard()
+}
 function createBoard(){
     console.log("Making Board")
     var board = document.getElementById("chessboard");
@@ -9,11 +15,22 @@ function createBoard(){
         for(let col=0; col<size; col++){
             let cell = document.createElement("div");
             cell.classList.add("chessCell")
-            if((col+row) % 2 === 0) cell.classList.add("even");
-            else cell.classList.add("odd");
+            if((col+row) % 2 === 0) cell.classList.add("chessEven");
+            else cell.classList.add("chessOdd");
             rowElement.appendChild(cell);
         }
         board.appendChild(rowElement);
     }
+    updateColors()
+
+}
+
+function updateColors(){
+    let chessOdd =  document.getElementsByClassName("chessOdd");
+    let chessEven = document.getElementsByClassName("chessEven")
+    let oddColor = document.getElementById("chessOddColor").value;
+    let evenColor = document.getElementById("chessEvenColor").value;
+    for(let i=0; i<chessOdd.length; i++) chessOdd.item(i).style.backgroundColor = oddColor;
+    for(let i=0; i<chessEven.length; i++) chessEven.item(i).style.backgroundColor = evenColor;
 
 }
